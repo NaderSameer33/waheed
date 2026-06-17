7dc1ed1ff0246718a33b3c42c692ec732aa57fa4
nader <nadersameer@std.mans.edu.eg>
2026-06-17 15:12:02 +0300

    Finish of Catoegory Filtiring

--- lib/core/shared/widgets/custom_bottom_sheet.dart
+++ lib/core/shared/widgets/custom_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:waheed/core/extensions/sizedbox_extenstion.dart';
+import 'package:waheed/core/shared/utils/app_colors.dart';
 
 import 'package:waheed/core/shared/widgets/bottom_sheet_header.dart';
 
@@ -25,9 +26,76 @@ class CustomBottomSheet extends StatelessWidget {
           children: [
             33.vs,
             BottomSheetHeader(),
+            16.vs,
+            BottomSheetCategories(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class BottomSheetCategories extends StatefulWidget {
+  const BottomSheetCategories({
+    super.key,
+  });
+
+  @override
+  State<BottomSheetCategories> createState() => _BottomSheetCategoriesState();
+}
+
+class _BottomSheetCategoriesState extends State<BottomSheetCategories> {
+  final _list = ['الكل', 'الأحذية', 'الاكسسوارات', 'الاكسسوارات', 'القمصان'];
+  int currentIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'الأقسام',
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            fontSize: 16.sp,
+          ),
+        ),
+        Wrap(
+          spacing: 20.w,
+          runSpacing: 12.h,
+          children: [
+            ...List.generate(
+              5,
+              (index) => GestureDetector(
+                onTap: () {
+                  currentIndex = index;
+                  setState(() {});
+                },
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  padding: EdgeInsets.all(12.r),
+                  height: 40.h,
+                  decoration: BoxDecoration(
+                    color: currentIndex == index
+                        ? Colors.black
+                        : AppColors.scaffoldColor,
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+                  child: Text(
+                    _list[index],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.sp,
+                      color: currentIndex == index
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
