c622cb81dc575cb2791bcb36ec96afc70f62d35a
nader <nadersameer@std.mans.edu.eg>
2026-06-25 02:51:53 +0300

    finish of Recive from shop

--- lib/_features/checkout/presentation/widgets/recive_from_shop.dart
+++ lib/_features/checkout/presentation/widgets/recive_from_shop.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:waheed/_features/cart/presentation/widgets/cart_summary_order.dart';
+import 'package:waheed/_features/checkout/presentation/widgets/home_drive_cash.dart';
 import 'package:waheed/_features/checkout/presentation/widgets/recive_details_shop.dart';
+import 'package:waheed/core/extensions/sizedbox_extenstion.dart';
 
 class ReciveFromShop extends StatelessWidget {
   const ReciveFromShop({super.key});
@@ -9,6 +12,10 @@ class ReciveFromShop extends StatelessWidget {
     return Column(
       children: [
         ReciveDetailsShop(),
+        24.vs,
+        HomeDriveCash(),
+        24.vs,
+        CartSummaryOrder(),
       ],
     );
   }
